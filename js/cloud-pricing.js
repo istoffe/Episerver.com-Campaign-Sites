cd11ab3ddc4ddde77410ae6c2ee49cd3c8939631
Karl Löfholm <karllofholm@Karls-MacBook-Pro.local>
2017-01-22 11:11:54 +0100

    + Comparison table  - Mobile (CSS / HTML / JS) + Browser test (Chrome, Firefox, Safari) + Browser test - Mobile (iOS Safari)
    + Intro and tabs  - Desktop (CSS / HTML / JS)
    + Browser test (Chrome, Firefox, Safari)
    + Browser test - Mobile (iOS Safari)
    
    - Browser test whole page - Desktop (Chrome, Firefox, Safari)
    - Browser test whole page - Desktop (IE)
    - Browser test whole page - Mobile (iOS Safari)
    - Browser test whole page - Mobile (Android Chrome)
    
    + JS feature: Add feature info popover dynamically
    - How can we edit the content in a good way?
    
    - Special case for Packages feature: other companies
    - Fix all font sizes
    - Add headline under comparison table tabs on mobile
    + Fix div last child thing for highlighted rows
    + Add div in last highlighted tr row with JS
    + Make nice button animations
    - JS feature: Make all JS animations nice and smooth

--- js/cloud-pricing.js
+++ js/cloud-pricing.js
@@ -1,11 +1,12 @@
 (function ($) {
   var $wrapper = $('.js-cloud-pricing');
   if ( $wrapper.length == 0 ) return;
+
   //
   //
-  // .js-package-features
+  // .js-popover
   //
-  var togglePopover = function(){
+  var initPopovers = function(){
     var $popovers = $('.js-popover');
     $popovers.on('click', function(e){
       e.stopPropagation();
@@ -19,12 +20,50 @@
       $popovers.not($currentPopover).hide();
       $currentPopover.toggle();
     });
-
   }
-  togglePopover();
+  var setUpPopovers = function(){
+    var $popoverTemplate = $('.js-popover-template').clone().remove();
+    // var $popoverContent = $('.js-popover-content').replaceWith($popoverTemplate);
+    $('.js-popover-content').each(function(){
+      var $t = $(this);
+      var $popoverTemplateClone = $popoverTemplate.clone();
+      var popoverText = $t.text();
+      $popoverTemplateClone.find('.js-popover-text').text(popoverText);
+      $t.replaceWith($popoverTemplateClone);
+    });
+    initPopovers();
+  }
 
   //
   //
-  // .some-other-module
+  // .js-comparison-tables-tabs
   //
+  var comparisonTableTabs = function(){
+    var $wrapper = $('.js-comparison-tables');
+    var $tabs = $wrapper.find('.js-comparison-tables-tabs button');
+    var $cells = $wrapper.find("[class*='js-price-lv-']");
+
+    $tabs.on('click', function(){
+      var $t = $(this);
+      var cellTargetClass = $t.data('target');
+
+      $cells.removeClass('is-active');
+      $cells.filter( cellTargetClass ).addClass('is-active');
+
+      $tabs.removeClass('is-active');
+      $t.addClass('is-active')
+    });
+  }
+
+  //
+  //
+  // .js-add-attention-icon
+  //
+  var addAttentionIconForHighlightedRow = function(){
+    $('.js-add-attention-icon td').last().wrapInner('<div/>')
+  }
+
+  setUpPopovers();
+  comparisonTableTabs();
+  addAttentionIconForHighlightedRow();
 }(jQuery));
